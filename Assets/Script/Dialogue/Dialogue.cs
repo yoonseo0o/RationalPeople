b706fcc39126625faadf28e2672f50c6c177a36f
yoonseo0o <muddy0618@naver.com>
2024-03-29 20:11:01 +0900

    dialogue system

---
+++ Assets/Script/Dialogue/Dialogue.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Dialogue
+{
+    [Tooltip("대사 치는 캐릭터 이름")]
+    public string name;
+
+    [Tooltip("대사 내용")]
+    public string[] contexts;
+}
+
+public class DialogueEvent
+{
+    public string name; // 대화 시나리오 이름?
+    public Vector2 line; // x줄~y줄까지 출력
+    public Dialogue[] dialogues;  
+}
